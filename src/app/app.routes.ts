a6efad6031cf3a84b34339de432e883c10d23d59
carlsavila <carlsavila@gmail.com>
2024-06-16 19:38:40 +0200

    Commit folder Structure, rooting and main frame header, main, footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ItemsComponent } from './pages/items/items.component';
+import { KittensComponent } from './pages/kittens/kittens.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'items',
-    component: ItemsComponent,
-  },
+  {    path: 'home',    component: HomeComponent,  },
+  {    path: 'menu',    component: MenuComponent,  },
+  {    path: 'kittens',    component: KittensComponent,  },
+  {    path: 'signin',    component: SigninComponent,  },
+  {    path: 'signup',    component: SignupComponent,  },
+  {    path: 'contact',    component: ContactComponent,  },
+  {    path: 'about',    component: AboutComponent,  },
+  {    path: 'items',    component: ItemsComponent,  },
+  {    path: '',    redirectTo: '/home',    pathMatch: 'full',  },
 ];
---
+++ src/app/pages/kittens/kittens.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kittens',
+  standalone: true,
+  imports: [],
+  templateUrl: './kittens.component.html',
+  styleUrl: './kittens.component.scss'
+})
+export class KittensComponent {
+
+}
